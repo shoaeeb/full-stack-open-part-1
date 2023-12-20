f9495404160ae5689031133496518d6f87fd565c
shoaeeb <osman.shoaeeb@gmail.com>
2023-12-20 21:36:58 +0530

    unicafe step5

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -17,34 +17,28 @@ function App() {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button text={"good"} onClick={() => setGood(good + 1)} />
+      <Button text={"neutral"} onClick={() => setNeutral(neutral + 1)} />
+      <Button text={"bad"} onClick={() => setBad(bad + 1)} />
       {good + neutral + bad === 0 ? (
         <p>No feedback given</p>
       ) : (
-        <Statistics
-          average={average()}
-          positive={positive()}
-          good={good}
-          bad={bad}
-          neutral={neutral}
-        />
+        <>
+          <Statistics text={"all"} stats={good + bad + neutral} />
+          <Statistics text={"positive"} stats={positive()} />
+          <Statistics text={"average"} stats={average()} />
+        </>
       )}
     </div>
   );
 }
-
-function Statistics({ average, positive, good, neutral, bad }) {
+function Button({ text, onClick }) {
+  return <button onClick={onClick}>{text}</button>;
+}
+function Statistics({ text, stats }) {
   return (
     <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      {text} {stats}
     </div>
   );
 }
